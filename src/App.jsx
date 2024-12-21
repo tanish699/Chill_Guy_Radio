fdf3030b35b69c6825f7ebe498497dd5f455b991
tanish699 <tanishwadhwa2310@gmail.com>
2024-12-21 12:32:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/sidebar'
+
+
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'></div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/assets.js
@@ -0,0 +1,182 @@
+import bell_icon from './frontend-assets/bell.png';
+import home_icon from './frontend-assets/home.png';
+import like_icon from './frontend-assets/like.png';
+import loop_icon from './frontend-assets/loop.png';
+import mic_icon from './frontend-assets/mic.png';
+import next_icon from './frontend-assets/next.png';
+import play_icon from './frontend-assets/play.png';
+import pause_icon from './frontend-assets/pause.png';
+import plays_icon from './frontend-assets/plays.png';
+import prev_icon from './frontend-assets/prev.png';
+import search_icon from './frontend-assets/search.png';
+import shuffle_icon from './frontend-assets/shuffle.png';
+import speaker_icon from './frontend-assets/speaker.png';
+import stack_icon from './frontend-assets/stack.png';
+import zoom_icon from './frontend-assets/zoom.png';
+import plus_icon from './frontend-assets/plus.png';
+import arrow_icon from './frontend-assets/arrow.png';
+import mini_player_icon from './frontend-assets/mini-player.png';
+import queue_icon from './frontend-assets/queue.png';
+import volume_icon from './frontend-assets/volume.png';
+import arrow_right from './frontend-assets/right_arrow.png';
+import arrow_left from './frontend-assets/left_arrow.png';
+import spotify_logo from './frontend-assets/spotify_logo.png';
+import clock_icon from './frontend-assets/clock_icon.png';
+import img1 from './frontend-assets/img1.jpg';
+import img2 from './frontend-assets/img2.jpg';
+import img3 from './frontend-assets/img3.jpg';
+import img4 from './frontend-assets/img4.jpg';
+import img5 from './frontend-assets/img5.jpg';
+import img6 from './frontend-assets/img6.jpg';
+import img7 from './frontend-assets/img7.jpg';
+import img8 from './frontend-assets/img8.jpg';
+import img9 from './frontend-assets/img9.jpg';
+import img10 from './frontend-assets/img10.jpg';
+import img11 from './frontend-assets/img11.jpg';
+import img12 from './frontend-assets/img12.jpg';
+import img13 from './frontend-assets/img13.jpg';
+import img14 from './frontend-assets/img14.jpg';
+import img15 from './frontend-assets/img15.jpg';
+import img16 from './frontend-assets/img16.jpg';
+import song1 from './frontend-assets/song1.mp3';
+import song2 from './frontend-assets/song2.mp3';
+import song3 from './frontend-assets/song3.mp3';
+
+export const assets = {
+  bell_icon,
+  home_icon,
+  like_icon,
+  loop_icon,
+  mic_icon,
+  next_icon,
+  play_icon,
+  pause_icon,
+  plays_icon,
+  prev_icon,
+  search_icon,
+  shuffle_icon,
+  speaker_icon,
+  stack_icon,
+  zoom_icon,
+  plus_icon,
+  arrow_icon,
+  mini_player_icon,
+  queue_icon,
+  volume_icon,
+  arrow_left,
+  arrow_right,
+  spotify_logo,
+  clock_icon,
+};
+
+export const albumsData = [
+  {
+    id: 0,
+    name: "Top 50 Global",
+    image: img8,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#2a4365",
+  },
+  {
+    id: 1,
+    name: "Top 50 India",
+    image: img9,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#22543d",
+  },
+  {
+    id: 2,
+    name: "Trending India",
+    image: img10,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#742a2a",
+  },
+  {
+    id: 3,
+    name: "Trending Global",
+    image: img16,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#44337a",
+  },
+  {
+    id: 4,
+    name: "Mega Hits",
+    image: img11,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#234e52",
+  },
+  {
+    id: 5,
+    name: "Happy Favorites",
+    image: img15,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#744210",
+  },
+];
+
+export const songsData = [
+  {
+    id: 0,
+    name: "Song One",
+    image: img1,
+    file: song1,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "3:00",
+  },
+  {
+    id: 1,
+    name: "Song Two",
+    image: img2,
+    file: song2,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:20",
+  },
+  {
+    id: 2,
+    name: "Song Three",
+    image: img3,
+    file: song3,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:32",
+  },
+  {
+    id: 3,
+    name: "Song Four",
+    image: img4,
+    file: song1,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:50",
+  },
+  {
+    id: 4,
+    name: "Song Five",
+    image: img5,
+    file: song2,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "3:10",
+  },
+  {
+    id: 5,
+    name: "Song Six",
+    image: img14,
+    file: song3,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:45",
+  },
+  {
+    id: 6,
+    name: "Song Seven",
+    image: img7,
+    file: song1,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:18",
+  },
+  {
+    id: 7,
+    name: "Song Eight",
+    image: img12,
+    file: song2,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:35",
+  },
+];
