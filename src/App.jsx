c88baba0538ceb5d2527800c39cdf8b90c4a5d96
tanish699 <tanishwadhwa2310@gmail.com>
2024-12-21 13:14:17 +0530

    adding the complete sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import Sidebar from './components/sidebar'
 const App = () => {
   return (
     <div className='h-screen bg-black'>
-      <div className='h-[90%] flex'></div>
-      <Sidebar />
+      <div className='h-[90%] flex'>
+        <Sidebar />
+      </div>
+      
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,9 +6,35 @@ const Sidebar = () => {
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
       <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-           <img src={assets.home_icon} alt="" />
-         
-       </div>
+           <img className='w-6' src={assets.home_icon} alt="" />
+           <p className='font-bold'>Home</p>
+        </div>
+        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+           <img className='w-6' src={assets.search_icon} alt="" />
+           <p className='font-bold'>Search</p>
+        </div>
+      </div>
+      <div className='bg-[#121212] h-[85%] rounded'>
+        <div className='p-4 flex items-center justify-between'>
+             <div className='flex items-center gap-3'>
+                <img className='w-8' src={assets.stack_icon} alt="" />
+                <p className='font-semibold'>Your Library</p>
+             </div>
+            <div className='flex items-center gap-3'>
+                <img className='w-5' src={assets.arrow_icon} alt="" />
+                <img className='w-5' src={assets.plus_icon} alt="" />
+            </div>
+        </div>
+        <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gdp-1 pl-4'>
+          <h1>Create Your First Playlist</h1>
+          <p className='font-light'>Nahi aati bnani?</p>
+          <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+        </div>
+        <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gdp-1 pl-4 mt-4'>
+          <h1>Let's findsome podcasts to follow</h1>
+          <p className='font-light'>Updates aate rahe ge</p>
+          <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
+        </div>
       </div> 
     </div>
   )
