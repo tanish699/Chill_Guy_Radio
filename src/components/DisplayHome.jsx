57388cecab61ca7d53f0f9c6de2e7648182bf373
tanish699 <tanishwadhwa2310@gmail.com>
2025-01-01 19:54:15 +0530

    adding Albums to Home Screen

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import { albumsData } from '../assets/assets'
+import Albumitem from './Albumitem'
+import { songsData } from '../assets/assets'
+import Songsitem from './Songsitem'
 
 const DisplayHome = () => {
   return (
     <>
      <Navbar />
-     <div className='my-5 font-bold text-2xl'>
-      
-
+     <div className='mb-4 '>
+      <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+      <div className='flex overflow-auto'>
+        {albumsData.map((item,index)=>(<Albumitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+      </div>
+     </div>
+     <div className='mb-4 '>
+      <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
+      <div className='flex overflow-auto'>
+        {songsData.map((item,index)=>(<Songsitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+      </div>
      </div>
     </>
   )
